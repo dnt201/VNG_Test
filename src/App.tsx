3d814de0efacd19adc0c4b93451bd2fb2e022627
dnt.201 <89123004+dnt201@users.noreply.github.com>
2023-02-24 10:01:48 +0700

    Remove console.log, not use import

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { redirect } from "react-router-dom";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
 import NavLeft from "./modules/NavLeft";
 
 const App = () => {
   const [_mini, _setMini] = useState(false);
   const [_lazy, _setLazy] = useState(false);
-  useEffect(() => {
-    <Navigate to="/" replace />;
-  }, []);
+
   return (
     <div className="flex relative h-screen ">
       <NavLeft
